b168b3ff04529b5c2f1e5c593efac032922b283a
Markus Dübbert <info@markusduebbert.de>
2023-08-07 12:51:12 +0200

    46554 bravo ce package

--- Classes/Configuration/Extension.php
+++ Classes/Configuration/Extension.php
@@ -36,7 +36,6 @@ final class Extension
         SI::ICON_CONTENT_CONTAINER_COLUMNS_2_RIGHT => 'EXT:bravo_content_elements/Resources/Public/Backend/Icons/content-container-columns-2-right.svg',
         SI::ICON_CONTENT_CONTAINER_COLUMNS_3 => 'EXT:bravo_content_elements/Resources/Public/Backend/Icons/content-container-columns-3.svg',
         SI::ICON_CONTENT_CONTAINER_COLUMNS_4 => 'EXT:bravo_content_elements/Resources/Public/Backend/Icons/content-container-columns-4.svg',
-        SI::ICON_CONTENT_ACCORDION_ITEM => 'EXT:bravo_content_elements/Resources/Public/Backend/Icons/content-accordion-item.svg'
     ];
     protected const ADD_PAGE_TSCONFIG = [];
     protected const ADD_RTE_PRESETS = [
--- Classes/Configuration/SettingsInterface.php
+++ Classes/Configuration/SettingsInterface.php
@@ -19,4 +19,21 @@ interface SettingsInterface
     public const NAME = 'BravoContentElements';
     public const KEY = 'bravo_content_elements';
     public const VENDOR_NAME = 'Cpsit';
+
+    public const ICON_IDENTIFIER_PAGE_KNOWLEDGE_COMPASS = 'page-knowledge-compass';
+    public const ICON_IDENTIFIER_PAGE_KNOWLEDGE_COMPASS_ROOT = 'page-knowledge-compass';
+    public const ICON_IDENTIFIER_PAGE_KNOWLEDGE_COMPASS_HIDE_IN_MENU = 'page-knowledge-compass-hide-in-menu';
+    public const ICON_IDENTIFIER_PAGE_SHOWCASE = 'page-showcase';
+    public const ICON_IDENTIFIER_PAGE_SHOWCASE_ROOT = 'page-showcase';
+    public const ICON_IDENTIFIER_PAGE_SHOWCASE_HIDE_IN_MENU = 'page-showcase-hide-in-menu';
+    public const ICON_IDENTIFIER_PAGE_SHOWCASE_PROMOTION = 'page-showcase-promotion';
+    public const ICON_IDENTIFIER_PAGE_SHOWCASE_PROMOTION_ROOT = 'page-showcase-promotion';
+    public const ICON_IDENTIFIER_PAGE_SHOWCASE_PROMOTION_HIDE_IN_MENU = 'page-showcase-promotion-hide-in-menu';
+    public const ICON_IDENTIFIER_CONTENT_CONTACT = 'contact';
+    public const ICON_CONTENT_CONTAINER_COLUMNS_2 = 'content-container-columns-2';
+    public const ICON_CONTENT_CONTAINER_COLUMNS_2_LEFT = 'content-container-columns-2-left';
+    public const ICON_CONTENT_CONTAINER_COLUMNS_2_RIGHT = 'content-container-columns-2-right';
+    public const ICON_CONTENT_CONTAINER_COLUMNS_3 = 'content-container-columns-3';
+    public const ICON_CONTENT_CONTAINER_COLUMNS_4 = 'content-container-columns-4';
+
 }
