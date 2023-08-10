7a9ed0840513c730badc4bee577b036ba6e13c71
blueamerican <blueamerican@web.de>
2023-08-10 08:50:16 +0200

    46554 bravo ce package

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,13 +3,13 @@ defined('TYPO3') or die();
 
 (function ($extKey = 'bravo_content_elements') {
 
-    //Custom config for RTE - fckeditor
-//    $rtePresets = ['Default', 'FrDefault', 'FrMinimal'];
-//
-//    foreach ($rtePresets as $rtePreset) {
-//        $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets'][$rtePreset] =
-//            'EXT:' . $extKey . '/Configuration/RTE/' . $rtePreset . '.yaml';
-//    }
+    #Custom config for RTE - fckeditor
+    $rtePresets = ['Default', 'FrDefault', 'FrMinimal'];
+
+    foreach ($rtePresets as $rtePreset) {
+        $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets'][$rtePreset] =
+            'EXT:' . $extKey . '/Configuration/RTE/' . $rtePreset . '.yaml';
+    }
 
 
 })();
