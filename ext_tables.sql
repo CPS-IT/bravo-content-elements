161a083614927cc7838b31869f188ed91352b959
Markus Dübbert <info@markusduebbert.de>
2023-11-23 14:02:16 +0100

    49095 add checkbox barriere free

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,3 +6,10 @@ CREATE TABLE tt_content
 	teaser_link       varchar(1024)    DEFAULT ''  NOT NULL,
 	teaser_link_label varchar(100)     DEFAULT ''  NOT NULL
 );
+
+# sys_file_metadata
+#
+CREATE TABLE sys_file_metadata
+(
+	accessibility tinyint(4) DEFAULT '0' NOT NULL
+);
